eedc05e32696b57fc17ac4b135c6628c9d72d8d2
SeaPickle <uraniumpjs@gmail.com>
2024-07-28 13:42:14 -0600

    Made first (logged) contact!

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import curses
 from datetime import datetime, timezone
 from curses.textpad import Textbox, rectangle
+from hamutils.adif import ADIWriter
+
 
 qso_data = {"QSO callsign":None, "time":None, "date":None, "Sent signal":None, "Received signal": None, "Frequency": None, "Mode":None}  
 
@@ -9,8 +11,9 @@ def init_curses():
     stdscr = curses.initscr()
     curses.noecho()
     curses.cbreak()
+    curses.start_color()
     stdscr.keypad(True)
-    
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_RED)
 def end_curses():
     curses.nocbreak()
     stdscr.keypad(False)
@@ -30,8 +33,15 @@ def get_input(x, y, width, height):
 
 #------------------------------------------
 
+def  init_program():
+    logscr = curses.newwin(curses.LINES // 2, curses.COLS, curses.LINES//2, 0)
+    logscr.clear()
+    logscr.addch(0,0,"a")
+    logscr.refresh()
+    logscr.getkey()
 def begin():
     init_curses()
+    #init_program()
     show_tutorial()
     query_for_presets()
     mainloop()
@@ -66,28 +76,52 @@ def query_for_presets():
                 if (stdscr.getkey() == "y"):
                    qso_data[data] = box
                    break
+    stdscr.clear()
+    stdscr.addstr(0,0, "Presets done, press key to start logging!", curses.color_pair(1))
+    stdscr.refresh()
+    stdscr.getkey()
+def convert_freq_to_band(freq):
+    freq_Mhz = float(freq)
+    freq_Mhz = int(freq_Mhz)
+    if freq_Mhz == 1:
+        return "160m"
+    elif freq_Mhz == 3:
+        return "80m"
+    elif freq_Mhz == 5:
+        return "60m"
+    elif freq_Mhz == 7:
+        return "40m"
+    elif freq_Mhz == 10:
+        return "30m"
+    elif freq_Mhz == 14:
+        return "20m"
+    elif freq_Mhz == 18:
+        return "17m"
+    elif freq_Mhz == 21:
+        return "15m"
+    elif freq_Mhz == 24:
+        return "12m"
+    elif freq_Mhz == 28 or freq_Mhz == 29:
+        return "10m"
 
-#Not loop but whatever
+
 def mainloop():
     for data in qso_data.keys(): 
             if qso_data[data] != None:
                 continue
+            date = datetime.now(timezone.utc)
+            if data == "time":
+                qso_data[data] = date
+                continue
+            
             # this loop should continue until the user says the data is correct
             while(1):
                 stdscr.clear()
-                date = datetime.now(timezone.utc)
-                if data == "time":
-                    date = date.time()
-                    stdscr.addstr(0, 0, f"Enter {data} CTRL+G to end, currently {date.hour}:{date.minute} UTC", curses.A_STANDOUT)
-                if data == "date":
-                    date = date.date()
-                    stdscr.addstr(0,0, f"Enter {data} CTRL+G to end, currently {date.day}/{date.month} (D/M)", curses.A_STANDOUT)
-                else:
-                    stdscr.addstr(0, 0, f"Enter {data} CTRL+G to end:", curses.A_STANDOUT)
+
+                stdscr.addstr(0, 0, f"Enter {data} CTRL+G to end:", curses.A_STANDOUT)
                 box = get_input(1, 0, 7, 1) 
                 box = box.strip().upper()
-                if len(box) == 0:
-                   break
+                
                 stdscr.addstr(3,0, f"{data} is: {box} , y/n?", curses.A_BLINK)
                 stdscr.refresh()
                 key = stdscr.getkey()
@@ -97,4 +131,10 @@ def mainloop():
                 # ctrl+p == redo presets
                 if (ord(key[0]) == 16):
                     query_for_presets()
+
+    adi = ADIWriter(open("test.adif", "wb"))
+    adi.add_qso(call = qso_data["QSO callsign"], datetime_on = qso_data["time"], band = convert_freq_to_band(qso_data["Frequency"]), mode = qso_data["Mode"])
+    stdscr.addstr((curses.LINES - 1), 0, "Contacts: 1", curses.color_pair(1))
+    stdscr.refresh()
+    stdscr.getkey()
 begin()
